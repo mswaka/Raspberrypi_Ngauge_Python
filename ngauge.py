d20d661685efafc943735d6b5f05c7ff63a2fdf9
chimo <51476215+mswaka@users.noreply.github.com>
2020-01-27 14:20:16 +0900

    Update ngauge.py
    20200127 change コメント追記

--- ngauge.py
+++ ngauge.py
@@ -6,42 +6,42 @@ GPIO.setmode(GPIO.BCM)
 # pin9 is GND
 GPIO17 = 17  # pin11
 GPIO27 = 27  # pin13
-DUTY1 = 10
-DUTY2 = -10
+DUTY1 = 1
+DUTY2 = -1
 #OUT
 GPIO.setup(GPIO17, GPIO.OUT)
 GPIO.setup(GPIO27, GPIO.OUT)
 
 #PWM sets
-pwm1 = GPIO.PWM(GPIO17, 60)
-pwm2 = GPIO.PWM(GPIO27, 60)
+pwm1 = GPIO.PWM(GPIO17, 60) # frequency 60Hz
+pwm2 = GPIO.PWM(GPIO27, 60) # frequency 60Hz
 pwm1.start(0)
 pwm2.start(0)
 
 # run
-for dc in range(0, 60, DUTY1):
+for dc in range(0, 21, DUTY1):
     pwm1.ChangeDutyCycle(dc)
     # print(dc)
     time.sleep(1)
 time.sleep(10)
-for dc in range(60, -1, DUTY2):
+for dc in range(20, -1, DUTY2):
     pwm1.ChangeDutyCycle(dc)
     # print(dc)
     time.sleep(1)
 
 # stop
-time.sleep(10)
+#time.sleep(10)
 
 # opposite
-for dc in range(0, 60, DUTY1):
-    pwm2.ChangeDutyCycle(dc)
-    # print(dc)
-    time.sleep(1)
-time.sleep(5)
-for dc in range(60, -1, DUTY2):
-    pwm2.ChangeDutyCycle(dc)
-    # print(dc)
-    time.sleep(1)
+#for dc in range(0, 60, DUTY1):
+#    pwm2.ChangeDutyCycle(dc)
+#    # print(dc)
+#    time.sleep(1)
+#time.sleep(5)
+#for dc in range(60, -1, DUTY2):
+#    pwm2.ChangeDutyCycle(dc)
+#    # print(dc)
+#    time.sleep(1)
 
 pwm1.stop()
 pwm2.stop()
